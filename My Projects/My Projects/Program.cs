3ef34fc3598ce7737f99ee065738fdcc6857a413
Seth Wolf <sethmwolf@outlook.com>
2020-01-17 18:54:48 -0500

    Commit

--- My Projects/My Projects/Program.cs
+++ My Projects/My Projects/Program.cs
@@ -10,6 +10,25 @@ namespace My_Projects
     {
         static void Main(string[] args)
         {
+
+            Console.WriteLine("Name Display");
+
+            Console.WriteLine("What is your first name:");
+
+            string firstName = Console.ReadLine();
+
+            Console.WriteLine("What is your last name:");
+
+            string lastName = Console.ReadLine();
+
+            Console.WriteLine("Your full name is: " + firstName + " " + lastName);
+
+            string dontCloseTheConsole = Console.ReadLine();
+
+        
+
+
+
         }
     }
 }

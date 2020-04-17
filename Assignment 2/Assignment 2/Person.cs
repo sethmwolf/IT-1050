be0a80947c2c141518e95ec636b0a3a26e0342ed
sethmwolf <59946477+sethmwolf@users.noreply.github.com>
2020-04-16 23:20:31 -0400

    f
    f

---
+++ Assignment 2/Assignment 2/Person.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Assignment_2
+{
+    class Person
+    {
+        public static double SumOfAllAges = 0;
+        public static int Count = 0;
+
+        public int Age;
+        public string FirstName;
+        public string LastName;
+        public Person Spouse;
+
+        public string GetFullName()
+        {
+            return this.FirstName + " " + this.LastName;
+        }
+
+        public void PrintNameAge()
+        {
+            System.Console.WriteLine(this.GetFullName() + " is " + this.Age + " years old.");
+          
+        }
+
+        public void GetInformation()
+        {
+            System.Console.WriteLine("What is your first name?:");
+            this.FirstName = System.Console.ReadLine();
+
+            System.Console.WriteLine("What is your last name?:");
+            this.LastName = System.Console.ReadLine();
+
+            System.Console.WriteLine("What is your age?:");
+            this.Age = int.Parse(System.Console.ReadLine());
+
+            Count++;
+
+            SumOfAllAges += this.Age;
+
+
+        }
+
+       
+        public void CreatePersonifMarried()
+        {
+            System.Console.WriteLine("Are you married?:");
+
+            string MarriedResponse = System.Console.ReadLine();
+
+            if (MarriedResponse.Equals("Yes") || MarriedResponse.Equals("yes") || MarriedResponse.Equals("Y") || MarriedResponse.Equals("y"))
+            {
+                this.Spouse = new Person();
+
+                this.Spouse.GetInformation();
+
+                this.Spouse.Spouse = this;
+
+             }
+
+        }
+
+      
+    }
+
+    
+}
---
+++ Assignment 2/Assignment 2/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Assignment_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            
+
+            Person p1 = new Person();
+            p1.GetInformation();
+            p1.CreatePersonifMarried();
+
+            Person p2 = new Person();
+            p2.GetInformation();
+            p2.CreatePersonifMarried();
+
+            p1.PrintNameAge();
+            p1.Spouse.PrintNameAge();
+            p2.PrintNameAge();
+            p2.Spouse.PrintNameAge();
+
+            double AverageAge = Person.SumOfAllAges / Person.Count;
+            Console.WriteLine("Average Age is " + AverageAge + " years old");
+
+            Console.WriteLine("Press any key . . .");
+            Console.ReadKey();
+
+        }
+    }
+}

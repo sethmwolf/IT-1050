4f96e46168b8d48eb50a050d08b28a266b8be396
Seth Wolf <sethmwolf@outlook.com>
2020-04-28 21:58:52 -0400

    Assignment 5

---
+++ Assignment 5/Assignment 5/Person.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Assignment_5
+{
+    class Person
+    {
+
+        private string FirstName;
+        private string LastName;
+        private string FullName;
+
+        public Person(string first, string last)
+        {
+
+            this.FirstName = first;
+            this.LastName = last;
+            this.FullName = this.FirstName + " " + this.LastName;
+
+        }
+
+        public void PrintFullName()
+
+        {
+
+            System.Console.WriteLine(this.FullName);
+
+        }
+
+
+    }
+}
---
+++ Assignment 5/Assignment 5/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+
+namespace Assignment_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int Count = 0;
+
+            while (Count < 3)
+
+            {
+
+                System.Console.WriteLine("How Many People In Your Group? ");
+                Count = int.Parse(System.Console.ReadLine());
+
+                if (Count < 3)
+
+                {
+                    
+                    System.Console.WriteLine("Sorry, your group must consist of at least 3 people.  Please try again." +Environment.NewLine);
+
+                }
+
+
+            }
+            
+            
+
+
+
+            Person[] Group = new Person[Count];
+
+            for (int i = 0; i<Count; i++)
+
+            {
+                System.Console.WriteLine("What is the First Name of Person #" + (i + 1) + "? ");
+                string FirstNameofArrayPerson = System.Console.ReadLine();
+
+                System.Console.WriteLine("What is the Last Name of Person #" + (i + 1) + "? ");
+                string LastNameofArrayPerson = System.Console.ReadLine();
+
+                Group[i] = new Person(FirstNameofArrayPerson, LastNameofArrayPerson);
+
+            }
+
+
+            for (int i = 0; i < Count; i++)
+
+            {
+
+                System.Console.WriteLine("The name of Person #" + (i + 1) + " is ");
+                Group[i].PrintFullName();
+
+            }
+
+            System.Console.ReadKey();
+
+
+        }
+    }
+}
